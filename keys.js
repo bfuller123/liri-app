4bcc96b279867056c15158fdf8b64a9403cc32b8
Brett Fuller <bfuller123@gmail.com>
2017-07-02 20:17:16 -0500

    added twitter keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'gRRdojaq2Qo3zNCz81grI3I5c',
+  consumer_secret: '2Gm1Oku3ZMJAUYwMSvlEgl9odbn7guoE1Ah078HsgZOoxFu4Yk',
+  access_token_key: '881671458723946496-3lpX5Ayq62hw2Owcdf2u7z1kHdF9YsX',
+  access_token_secret: 	'bGEVb5HbICJT0DtuI2DGa0DeWA8cEnuh2nMzhbJwzGkLb',
+}
